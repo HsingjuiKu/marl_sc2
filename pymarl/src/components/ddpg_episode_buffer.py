c603e64030d053f4ecec5e519f7e355ab50b4b92
Chuyi Jiang <157999719+chenchencc33@users.noreply.github.com>
2024-07-16 15:22:21 +0800

    Update ddpg_episode_buffer.py

--- pymarl/src/components/ddpg_episode_buffer.py
+++ pymarl/src/components/ddpg_episode_buffer.py
@@ -105,6 +105,9 @@ class EpisodeBatch:
                 raise KeyError("{} not found in transition or episode data".format(k))
 
             dtype = self.scheme[k].get("dtype", th.float32)
+
+            # Convert the list of numpy.ndarrays to a single numpy.ndarray
+            v = np.array(v)
             v = th.tensor(v, dtype=dtype, device=self.device)
             try:
                 self._check_safe_view(v, target[k][_slices])
