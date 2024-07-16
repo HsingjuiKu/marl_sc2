50f3de1b89f2554e70d41700708a2ac45b6b38de
Chuyi Jiang <157999719+chenchencc33@users.noreply.github.com>
2024-07-16 09:37:15 +0800

    Update maddpg_learner_discrete1.py

--- pymarl/src/learners/maddpg_learner_discrete1.py
+++ pymarl/src/learners/maddpg_learner_discrete1.py
@@ -4,6 +4,9 @@ from modules.critics.maddpg import MADDPGCritic
 import torch as th
 from torch.optim import RMSprop, Adam
 from utils.rl_utils import build_td_lambda_targets
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from redistribute import EnhancedCausalModel
 
 class MADDPGDiscreteLearner:
