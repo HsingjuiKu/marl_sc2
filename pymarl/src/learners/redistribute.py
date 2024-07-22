a47e3f49f2ebe716b895c90ffa831ee7afd1d1ca
Chuyi Jiang <157999719+chenchencc33@users.noreply.github.com>
2024-07-22 22:06:25 +0800

    Update redistribute.py

--- pymarl/src/learners/redistribute.py
+++ pymarl/src/learners/redistribute.py
@@ -57,7 +57,7 @@ class EnhancedCausalModel(nn.Module):
             influences.append(influence)
         
         influences = torch.stack(influences, dim=-1)
-        influences = influences.mean(dim = (1,2), keepdim =False)
+        influences = influences.mean(dim = 2, keepdim =False)
         print(influences.shape)
         l2_norm = influences.norm(p=2, dim=-1, keepdim=True)
         normalized_influences = influences / (l2_norm + 1e-8) 
