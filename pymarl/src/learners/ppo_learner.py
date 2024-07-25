34b3c0a031de71e6b705811bdbb78dfe583af8e4
Chuyi Jiang <157999719+chenchencc33@users.noreply.github.com>
2024-07-26 00:26:53 +0800

    Update ppo_learner.py

--- pymarl/src/learners/ppo_learner.py
+++ pymarl/src/learners/ppo_learner.py
@@ -121,7 +121,7 @@ class PPOLearner:
                     self.critic, self.target_critic, batch, agent_rewards, critic_mask
                 )
                 print(critic_train_stats)
-                print(advantage)
+                print(advantages)
                 all_advantages.append(advantages)
                 all_critic_train_stats.append(critic_train_stats)
 
