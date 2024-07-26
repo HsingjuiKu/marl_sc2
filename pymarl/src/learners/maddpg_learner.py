f4ffe8282204aefe670846848d0931d6ba4d25ec
Chuyi Jiang <157999719+chenchencc33@users.noreply.github.com>
2024-07-27 01:48:42 +0800

    Update maddpg_learner.py

--- pymarl/src/learners/maddpg_learner.py
+++ pymarl/src/learners/maddpg_learner.py
@@ -60,7 +60,7 @@ class MADDPGLearner:
         terminated = batch["terminated"][:, :-1].float()
         mask = batch["filled"][:, :-1].float()
         mask[:, 1:] = mask[:, 1:] * (1 - terminated[:, :-1])
-
+        print(mask.shape)
         obs = batch["obs"][:, :-1]
         # calculate social influence for all samples in this batch
         social_influence = self.redistribution_model.calculate_social_influence(obs, actions)
